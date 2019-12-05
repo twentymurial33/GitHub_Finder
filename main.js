e74bfdc8bfd7100ba2de28b40211c918a56bd39a
murial.anindo <murial.anindo@cloudistics.com>
2019-12-05 11:57:23 -0500

    commiting

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-   alert("Page clicked!")
+
+// Shorthand for $( document ).ready()
+$(function() {
+    $("#searchUser").on("keyup",function(){
+        console.log("e.target.value")
+    })
 })
+
+$.ajax({
+    url:"https://api.github.com/users/" +username,
+    data:{
+        client_id:" ",
+        client_secret:" "
+    }
+}) .done(function(user){
+  console.log(user);
+});
